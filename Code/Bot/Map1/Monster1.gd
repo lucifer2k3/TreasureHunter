8c46842c97b40f46f7065f3458842401a2d43e31
Anhprodev <dotuana244@gmail.com>
2024-06-07 21:20:46 +0700

    sound intro
    sound intro

--- Code/Bot/Map1/Monster1.gd
+++ Code/Bot/Map1/Monster1.gd
@@ -1,18 +1,10 @@
 extends Area2D
 @onready var main = %mainchar
-
-
-
 var speed = 100
 var direction = Vector2(-1, 0)
 var center_position = Vector2()  
 var left_limit = 0 
 var right_limit = 0  
-
-#stat
-var maxHealth = 100
-var maxDmg = 50
-
 func _ready():
 	center_position = position
 	left_limit = center_position.x - 100
@@ -27,4 +19,6 @@ func _physics_process(delta):
 		direction.x = 1 
 func _on_body_entered(body):
 	if(body.name=="mainchar"):
-		main.damaged(maxDmg)
+		main.damaged(3)
+func donothing():
+	print("ranh")
